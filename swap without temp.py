475ac8a651d679c50dcf43648e00e04c3d8cd785
Sahana devi R A <sahanadevi2037@gmail.com>
2026-04-12 19:46:52 +0530

    Add files via upload

---
+++ swap without temp.py
@@ -0,0 +1,17 @@
+a = int(input("Enter a :"))
+b = int(input("Enter b:"))
+
+print("Before swapping")
+
+print("a = ",a)
+print("b = ",b)
+
+a = a + b
+b = a - b
+a = a - b
+
+print("After swapping")
+
+print("a = ",a)
+print("b = ",b)
+
