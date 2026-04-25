b8de5c2e8f96327f58ecd0d179f9cb6e62898e38
Sahana devi R A <sahanadevi2037@gmail.com>
2026-04-25 23:20:29 +0530

    Add files via upload

---
+++ tonnes and pounds.py
@@ -0,0 +1,10 @@
+print("KILOGRAMS TO TONNES, MILES AND POUNDS")
+
+kilo = int(input("Enter the value in kilogram: "))
+pounds = 2.2*kilo
+tonnes = 0.001*kilo
+miles = 0.621*kilo
+
+print("The value in pounds is: ",pounds)
+print("The value in tonnes is: ",tonnes)
+print("The value in miles is: ",miles)
